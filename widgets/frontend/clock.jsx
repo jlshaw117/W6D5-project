1fd86339873d0e3862f1b43bcec338c5f18e81bd
jlshaw117 <jlshaw117@gmail.com>
2018-11-30 14:56:24 -0800

    Add tick, constructor, and ComponentDidMount() functions

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    
+    this.state = {time: new Date() };
+  }
+  
+  tick() {
+    this.setState({time: new Date()});
+  }
+  
+  componentDidMount() {
+    setInterval(this.tick, 1000);
+  }
+  
   render () {
     return <h1>Clock</h1>; 
   }
