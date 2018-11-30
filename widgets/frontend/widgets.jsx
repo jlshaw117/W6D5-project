9c3244983fac075f6c9446dc503d45509671b88d
jlshaw117 <jlshaw117@gmail.com>
2018-11-30 15:00:31 -0800

    Change rendering error message

--- widgets/frontend/widgets.jsx
+++ widgets/frontend/widgets.jsx
@@ -4,7 +4,7 @@ import Clock from './clock';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>call of duty is overrated</h1>, root);
+  ReactDOM.render(<h1>You done goofed A A ron</h1>, root);
   
   ReactDOM.render(
     <Clock />,
