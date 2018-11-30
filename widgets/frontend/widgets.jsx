eced90996b164292a9619658a3a1a2855946059a
jlshaw117 <jlshaw117@gmail.com>
2018-11-30 14:05:51 -0800

    Add event listener, for DOMContentLoaded, and pass in render function as callback

---
+++ widgets/frontend/widgets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Widget extends React.Component {
+  
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementsByClassName('root');
+  ReactDOM.render(<h1>call fo duty is overrated</h1>, root);
+});
+
+export default Widget;
