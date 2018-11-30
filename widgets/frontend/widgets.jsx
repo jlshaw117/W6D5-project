9e65c80a6d352c285127d4e169865da8c39fabca
jlshaw117 <jlshaw117@gmail.com>
2018-11-30 14:57:16 -0800

    Refactor

--- widgets/frontend/widgets.jsx
+++ widgets/frontend/widgets.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>call of duty is overrated</h1>, root);
